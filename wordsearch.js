9fd88f7f946d60ac0f7e56c236e8db1c35ee143f
Balkaran Jaswal <balkaranjaswal@gmail.com>
2020-07-30 02:00:05 +0000

    WordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,47 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const createMatrix = function(col, row) {
+  let resMatrix = [];
+  for (let i = 0; i < row; i++) {
+    resMatrix.push(new Array(col).fill("x"));
+  }
+  return resMatrix;
+};
+  
+const transpose = function(matrix) {
+  let row = matrix.length;
+  let col = matrix[0].length;
+  
+  let newMatrix = createMatrix(row, col);
+  
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      newMatrix[j][i] = matrix[i][j];
     }
-}
+  }
+  return newMatrix;
+};
+  
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (letters.length <= 0) {
+    return false;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const reverseJoin = letters.map(ls => ls.reverse().join(''));
+  for (let l of reverseJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
